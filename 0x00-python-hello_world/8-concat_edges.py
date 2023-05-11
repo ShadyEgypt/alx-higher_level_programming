1494bd83b01ec4389ab61d774b71e3e109086ee2
ShadyEgypt <shadyrafat60@gmail.com>
2023-05-11 03:38:30 +0300

    task 8: concatenate 2

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that is used in many fields"
-str = str[39:67] + str[107:112] + str[:6]
+str = str[39:66] + str[106:112] + str[:6]
 print("{}".format(str))
