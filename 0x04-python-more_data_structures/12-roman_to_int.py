92582cae70a81e310c2cc7f6524fda32ef8e00b2
ShadyEgypt <shadyrafat60@gmail.com>
2023-05-18 03:06:22 +0300

    convert roman to decimal

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        intVal = digits[roman]
+        total += intVal if total < intVal * 5 else -intVal
+    return total
